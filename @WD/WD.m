da39a998933ed76f968509d16d6437047d0dca87
PC-CW <channingwong@qq.com>
2018-04-19 19:27:16 +0800

    Simplified the LDE function

--- @WD/WD.m
+++ @WD/WD.m
@@ -12,11 +12,8 @@ classdef WD
         enhI = vesselEnhanceLDE(param);
         
         function enImg = contrastEnhance( inImg )
-            param_LDE.Img = WD.enhance( inImg, 5000);
-            param_LDE.vessel_type = 'bright';
+            param_LDE.Img = WD.enhance( inImg, 5000 );
             param_LDE.detector_orientation  = -90:15:90;
-            param_LDE.predictor_orientation = 0;
-            param_LDE.scale                 = 1;
             param_LDE.offset_factor         = 0.6;
             enImg = WD.vesselEnhanceLDE( param_LDE );
         end
